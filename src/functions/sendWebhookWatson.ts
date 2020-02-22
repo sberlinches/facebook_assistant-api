9c9b67a414fc17952223b8dd4d70034d8391b855
Sergio Berlinches <sergioberlinches@gmail.com>
2020-02-22 17:21:49 +0100

    Filename refactored

--- src/functions/sendWebhookWatson.ts
+++ src/functions/sendWebhookWatson.ts
@@ -1,4 +1,6 @@
-exports.handler = async (event) => {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
