454a26ec6b96fd5054c1fdd6289fe3d40a918abb
Sergio Berlinches <sergioberlinches@gmail.com>
2020-02-22 17:07:14 +0100

    Initial commit

---
+++ src/functions/checkHealth.ts
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Health.js!',
+            event: event,
+        }),
+    };
+};
