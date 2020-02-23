455f1f1f3e680a6d5b539503050d271f19d2fc16
Sergio Berlinches <sergioberlinches@gmail.com>
2020-02-23 14:59:53 +0100

    Added returned data documentation

--- src/functions/getSession.ts
+++ src/functions/getSession.ts
@@ -5,6 +5,7 @@ import { SessionController } from '../controllers/session.controller';
  * Gets the information collected in a particular session
  * @param {APIGatewayEvent} event
  * @param {Context} context
+ * @return {Promise<APIGatewayProxyResult>}
  */
 module.exports.handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
 
--- src/functions/getSessions.ts
+++ src/functions/getSessions.ts
@@ -5,6 +5,7 @@ import { SessionController } from '../controllers/session.controller';
  * Gets a list of all sessions
  * @param {APIGatewayEvent} event
  * @param {Context} context
+ * @return {Promise<APIGatewayProxyResult>}
  */
 module.exports.handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
 
