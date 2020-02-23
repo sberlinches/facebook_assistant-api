d568d0e961b6aa7a99f82d0560889ce9dca5963b
Sergio Berlinches <sergioberlinches@gmail.com>
2020-02-23 15:16:20 +0100

    Refactored Watson Assistant methods

---
+++ src/controllers/watsonAssistant.controller.ts
@@ -0,0 +1,89 @@
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { ObjectId } from 'mongodb';
+import { Session } from '../databases/watsonAssistant/interfaces/session.interface';
+import { WatsonAssistantDb } from '../databases/watsonAssistant/watsonAssistant.db';
+
+/**
+ * Watson Assistant controller
+ */
+export class WatsonAssistantController {
+
+    /**
+     * Receives and handles the Watson Assistant collected information.
+     * @param {APIGatewayEvent} event
+     * @return {Promise<APIGatewayProxyResult>}
+     */
+    public static handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+
+        if(!event.body) {
+            const message = 'Body required';
+            console.error('%o: %s', new Date(), message);
+            return {
+                statusCode: 500,
+                body: JSON.stringify(message),
+            }
+        }
+
+        try {
+            const body = JSON.parse(event.body);
+            await WatsonAssistantDb.connect();
+            let session: Session;
+
+            switch (body.method) {
+                case 'welcome':
+                    session = await WatsonAssistantController.sessionInitialization(body.localTime, body.timeZone);
+                    break;
+                case 'setPersonName':
+                    session = await WatsonAssistantController.setPersonName(body.id, body.name);
+                    break;
+            }
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify(session),
+            };
+        } catch (e) {
+            console.error('%o: %s', new Date(), e);
+            return {
+                statusCode: 500,
+                body: JSON.stringify(e),
+            }
+        }
+    };
+
+    /**
+     * Initializes the session document where all the extracted info in the
+     * current session will be stored.
+     * @param {string} localTime — The local time where the person is located
+     * @param {string} timeZone — The timezone where the person is located
+     * @return {Promise<Session>}
+     */
+    private static sessionInitialization = async (localTime: string, timeZone: string): Promise<Session> => {
+
+        const session: Session = {
+            person: {
+                localTime: localTime,
+                timeZone: timeZone,
+            },
+            createAt: new Date(),
+        };
+
+        await WatsonAssistantDb.session.insertOne(session);
+
+        return session;
+    };
+
+    /**
+     * Adds the person name to the session document.
+     * @param {string} id — The session ID
+     * @param {string} name — The name of the person
+     * @return {Promise<Session>}
+     */
+    private static setPersonName = async (id: string, name: string): Promise<Session> => {
+
+        const updatedSession = await WatsonAssistantDb.session
+            .setPersonNameById(new ObjectId(id), name);
+
+        return updatedSession.value;
+    };
+}
--- src/functions/sendWebhookWatson.ts
+++ src/functions/sendWebhookWatson.ts
@@ -1,82 +1,15 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { ObjectId } from 'mongodb';
-import { WatsonAssistantDb } from '../databases/watsonAssistant/watsonAssistant.db';
-import { Session } from '../databases/watsonAssistant/interfaces/session.interface';
+import { APIGatewayProxyResult, APIGatewayEvent, Context } from 'aws-lambda';
+import { WatsonAssistantController } from '../controllers/watsonAssistant.controller';
 
 /**
- *
- * @param event
- * @param context
- * @param callback
+ * Receives and handles the Watson Assistant collected information
+ * @param {APIGatewayEvent} event
+ * @param {Context} context
+ * @return {Promise<APIGatewayProxyResult>}
  */
-module.exports.handler = (event, context, callback): APIGatewayProxyHandler => {
-
-    if(!event.body) {
-        const message = 'Body required';
-        console.error('%o: %s', new Date(), message);
-        return callback(message);
-    }
+module.exports.handler = (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
 
     context.callbackWaitsForEmptyEventLoop = false;
-
-    WatsonAssistantDb.connect()
-        .then(() => {
-            const body = JSON.parse(event.body);
-
-            switch (body.method) {
-                case 'welcome':
-                    return welcomeDialog(body.localTime, body.timeZone);
-                case 'setPersonName':
-                    return setPersonName(body.id, body.name);
-                default:
-                    return {};
-            }
-        })
-        .then((session) => {
-            return callback(null, {
-                statusCode: 200,
-                body: JSON.stringify(session),
-            });
-        })
-        .catch((err) => {
-            console.error('%o: %s', new Date(), err);
-            return callback(err);
-        });
+    return WatsonAssistantController.handler(event)
 };
 
-/**
- * Welcome Dialog
- * Creates for the very first time the session document where all the extracted
- * info in the current session will be stored.
- * @param {string} localTime
- * @param {string} timeZone
- * @return {Promise<Session>}
- */
-const welcomeDialog = async (localTime: string, timeZone: string): Promise<Session> => {
-
-    const session: Session = {
-        person: {
-            localTime: localTime,
-            timeZone: timeZone,
-        },
-        createAt: new Date(),
-    };
-
-    await WatsonAssistantDb.session.insertOne(session);
-
-    return session;
-};
-
-/**
- *
- * @param {string} id
- * @param {string} name
- * @return {Promise<Session>}
- */
-const setPersonName = async (id: string, name: string): Promise<Session> => {
-
-    const updatedSession = await WatsonAssistantDb.session
-        .setPersonNameById(new ObjectId(id), name);
-
-    return updatedSession.value;
-};
